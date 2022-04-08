4db1984b595dde93ce9478e80a910161bd397b10
Dr-Electron <dr-electr0n@protonmail.com>
2022-04-08 23:49:24 +0200

    Add Hornet with new build system

--- content/build/docusaurus.config.js
+++ content/build/docusaurus.config.js
@@ -4,6 +4,7 @@ const bee = require('./bee/docusaurus.config');
 const chronicle = require('./chronicle.rs/docusaurus.config');
 const chrysalis_docs = require('./chrysalis-docs/docusaurus.config');
 const goshimmer = require('./goshimmer/docusaurus.config');
+const hornet = require('./hornet/docusaurus.config');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
@@ -15,12 +16,14 @@ module.exports = {
         ...bee.plugins, 
         ...chronicle.plugins,
         ...chrysalis_docs.plugins,
-        ...goshimmer.plugins
+        ...goshimmer.plugins,
+        ...hornet.plugins
     ],
     staticDirectories: [
         ...bee.staticDirectories,
         ...chronicle.staticDirectories,
         ...chrysalis_docs.staticDirectories,
-        ...goshimmer.staticDirectories
+        ...goshimmer.staticDirectories,
+        ...hornet.staticDirectories
     ],
 };
