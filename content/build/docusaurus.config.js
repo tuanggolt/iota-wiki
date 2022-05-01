6dda2add2ee88a2af5818b81c7abd527843c7a20
Dr-Electron <dr-electr0n@protonmail.com>
2022-05-01 22:18:47 +0000

    Add Hornet versioning

--- content/build/docusaurus.config.js
+++ content/build/docusaurus.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 const identity = require('./identity.rs/docusaurus.config');
+const hornet = require('./hornet/docusaurus.config');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
@@ -7,6 +8,6 @@ module.exports = {
     url: '/',
     baseUrl: '/',
     themes: ['@docusaurus/theme-classic'],
-    plugins: [...identity.plugins],
-    staticDirectories: [...identity.staticDirectories],
+    plugins: [...identity.plugins, ...hornet.plugins],
+    staticDirectories: [...identity.staticDirectories, ...hornet.staticDirectories],
 };
--- content/build/hornet/docusaurus.config.js
+++ content/build/hornet/docusaurus.config.js
@@ -1,10 +1,10 @@
 // @ts-check
-const dev = require('./dev/documentation/docusaurus.config');
-const stable = require('./0.5.0/documentation/docusaurus.config');
+const dev = require('./develop/documentation/docusaurus.config');
+const stable = require('./mainnet/documentation/docusaurus.config');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: 'Identity',
+    title: 'Hornet',
     url: '/',
     baseUrl: '/',
     themes: ['@docusaurus/theme-classic'],
